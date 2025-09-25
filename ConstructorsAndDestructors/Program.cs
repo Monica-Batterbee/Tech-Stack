5c8278a4fc1b966e7b42aba675ab4b111da8026e
bat24002645 <bat24002645@PC0019479>
2025-09-25 09:40:54 +0100

    Add project files.

---
+++ ConstructorsAndDestructors/Program.cs
@@ -0,0 +1,24 @@
+﻿public class ClassName
+{
+    int val;
+    //constructor
+    public ClassName()
+    {
+        val = 10;
+    }
+
+    //destructor
+    ~ClassName()
+    {
+        val = 0;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassName c = new ClassName();
+            Console.WriteLine(c.val);
+        }
+    }
+}
