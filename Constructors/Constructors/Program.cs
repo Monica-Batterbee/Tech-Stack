01ba202839bb7d6fa149d456d7b461609cbfe8ed
Monica-Batterbee <bat24002645@students.wnc.ac.uk>
2025-09-30 16:00:22 +0100

    C# constructors program

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ConstructorProgram
+{
+    class ConstructureClass
+    {
+        public string name, location;
+
+        public ConstructureClass()
+        {
+            name = "Alex Len";
+            location = "San Francisco";
+        }
+
+        public ConstructureClass(string a, string b)
+        {
+            name = a;
+            location = b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConstructureClass className = new ConstructureClass();
+            Console.WriteLine(className.name);
+            Console.WriteLine(className.location);
+        }
+    }
+}
